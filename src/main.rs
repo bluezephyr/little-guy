83acd2fe7cef67f6ed5f7231bfae817e7504a746
Blue Zephyr <blue.zephyr.git@gmail.com>
2025-09-16 22:19:28 +0200

    Updates to use hal

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,68 @@
+//! Blinks the LED on a Pico board
+//!
+//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
-use core::sync::atomic::{AtomicBool, Ordering};
-use cortex_m::peripheral::syst::SystClkSource;
-use cortex_m_rt::{entry, exception};
-use embedded_hal::digital::{OutputPin, PinState};
-use panic_probe as _;
-use rp2040_boot2;
+use embedded_hal::digital::OutputPin;
+use embedded_hal::delay::DelayNs;
+
+use cortex_m_rt::entry;
+// use panic_probe as _;
+use panic_halt as _;
+
+use hal::pac;
 use rp2040_hal as hal;
+
 use rtt_target::{rprintln, rtt_init_print};
 
+use rp2040_boot2;
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
-// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
-
-// Use an AtomicBool to allow access to the shared variable without using unsafe and without
-// disabling interrupts.
-static LED_ON: AtomicBool = AtomicBool::new(true);
-
-#[exception]
-fn SysTick() {
-    // Toggle the wanted LED state
-    // The `swap` method is not supported since thumbv6m only provide `load` and `store`
-    // operations. No support for CAS (Compare and Swap) operations, such as `swap`, etc.
-    LED_ON.store(!LED_ON.load(Ordering::Relaxed), Ordering::Relaxed);
-    rprintln!("Tick!");
-}
-
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Hello, world!");
+    rprintln!("Program start");
+    let mut pac = pac::Peripherals::take().unwrap();
+    // let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    let mut p = hal::pac::Peripherals::take().unwrap();
-    let cp = hal::pac::CorePeripherals::take().unwrap();
-
-    let sio = hal::Sio::new(p.SIO);
-    let pins = hal::gpio::Pins::new(p.IO_BANK0, p.PADS_BANK0, sio.gpio_bank0, &mut p.RESETS);
-    let mut led = pins.gpio22.into_push_pull_output();
-
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(p.WATCHDOG);
-
-    // Configure the clocks
-    let _ = hal::clocks::init_clocks_and_plls(
-        XTAL_FREQ_HZ,
-        p.XOSC,
-        p.CLOCKS,
-        p.PLL_SYS,
-        p.PLL_USB,
-        &mut p.RESETS,
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = hal::clocks::init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
         &mut watchdog,
     )
+    .ok()
     .unwrap();
 
-    // Configure the SysTick
-    let mut syst = cp.SYST;
-    syst.set_clock_source(SystClkSource::External);
-    syst.set_reload(hal::pac::SYST::get_ticks_per_10ms() * 50);
+    // let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut timer = rp2040_hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
-    syst.clear_current();
-    syst.enable_counter();
-    syst.enable_interrupt();
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
 
-    rprintln!("{} ticks per ms (times 50 modifier applied)", hal::pac::SYST::get_ticks_per_10ms());
+    let mut led_pin = pins.gpio22.into_push_pull_output();
 
     loop {
-        let _ = led.set_state(PinState::from(LED_ON.load(Ordering::Relaxed)));
+        rprintln!("on!");
+        led_pin.set_high().unwrap();
+        timer.delay_ms(500);
+        rprintln!("off!");
+        led_pin.set_low().unwrap();
+        timer.delay_ms(500);
     }
 }
